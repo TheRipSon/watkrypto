cd3ec81c609078793652225eea28a411c4692ad8
TheRipSon <mat0007@wp.pl>
2024-11-27 13:52:38 +0100

    Done

--- otp-vigenere/cryptoanalysis.py
+++ otp-vigenere/cryptoanalysis.py
@@ -121,84 +121,90 @@ if __name__ == "__main__":
     main()
 
 '''
-
+Estimated key is very close to orginal.
+Incresing message size give better results.
 '''
 
 
 '''
 (venv) user@user-HP-EliteBook-845-14-inch-G9-Notebook-PC:~/code/watKrypto/otp-vigenere$ python opt-vigenere.py 
-Key: 765d68c147ade3e9b5e374dc262d9d93298ebadad5f65a9e2e
+Key: aa06b3a6425d91c77f2cf94567e70d323aa200dfd714d9b660
 (venv) user@user-HP-EliteBook-845-14-inch-G9-Notebook-PC:~/code/watKrypto/otp-vigenere$ python cryptoanalysis.py 
 Key Length Estimation using Index of Coincidence:
-Key Length: 1, Average IC: 0.0068
-Key Length: 2, Average IC: 0.0067
-Key Length: 3, Average IC: 0.0068
-Key Length: 4, Average IC: 0.0065
-Key Length: 5, Average IC: 0.0165
-Key Length: 6, Average IC: 0.0065
-Key Length: 7, Average IC: 0.0061
-Key Length: 8, Average IC: 0.0065
-Key Length: 9, Average IC: 0.0066
-Key Length: 10, Average IC: 0.0162
-Key Length: 11, Average IC: 0.0063
-Key Length: 12, Average IC: 0.0060
-Key Length: 13, Average IC: 0.0065
-Key Length: 14, Average IC: 0.0054
-Key Length: 15, Average IC: 0.0163
+Key Length: 1, Average IC: 0.0064
+Key Length: 2, Average IC: 0.0064
+Key Length: 3, Average IC: 0.0063
+Key Length: 4, Average IC: 0.0063
+Key Length: 5, Average IC: 0.0178
+Key Length: 6, Average IC: 0.0062
+Key Length: 7, Average IC: 0.0062
+Key Length: 8, Average IC: 0.0063
+Key Length: 9, Average IC: 0.0064
+Key Length: 10, Average IC: 0.0175
+Key Length: 11, Average IC: 0.0062
+Key Length: 12, Average IC: 0.0059
+Key Length: 13, Average IC: 0.0064
+Key Length: 14, Average IC: 0.0059
+Key Length: 15, Average IC: 0.0175
 Key Length: 16, Average IC: 0.0061
-Key Length: 17, Average IC: 0.0067
-Key Length: 18, Average IC: 0.0063
-Key Length: 19, Average IC: 0.0059
-Key Length: 20, Average IC: 0.0158
-Key Length: 21, Average IC: 0.0055
-Key Length: 22, Average IC: 0.0058
-Key Length: 23, Average IC: 0.0059
+Key Length: 17, Average IC: 0.0060
+Key Length: 18, Average IC: 0.0059
+Key Length: 19, Average IC: 0.0061
+Key Length: 20, Average IC: 0.0172
+Key Length: 21, Average IC: 0.0059
+Key Length: 22, Average IC: 0.0061
+Key Length: 23, Average IC: 0.0062
 Key Length: 24, Average IC: 0.0059
-Key Length: 25, Average IC: 0.0765
-Key Length: 26, Average IC: 0.0063
-Key Length: 27, Average IC: 0.0061
-Key Length: 28, Average IC: 0.0050
-Key Length: 29, Average IC: 0.0054
-Key Length: 30, Average IC: 0.0151
-Key Length: 31, Average IC: 0.0055
-Key Length: 32, Average IC: 0.0052
-Key Length: 33, Average IC: 0.0051
-Key Length: 34, Average IC: 0.0060
-Key Length: 35, Average IC: 0.0143
-Key Length: 36, Average IC: 0.0055
-Key Length: 37, Average IC: 0.0054
-Key Length: 38, Average IC: 0.0055
-Key Length: 39, Average IC: 0.0054
-Key Length: 40, Average IC: 0.0150
-Key Length: 41, Average IC: 0.0054
-Key Length: 42, Average IC: 0.0043
-Key Length: 43, Average IC: 0.0050
-Key Length: 44, Average IC: 0.0050
-Key Length: 45, Average IC: 0.0153
-Key Length: 46, Average IC: 0.0051
-Key Length: 47, Average IC: 0.0050
-Key Length: 48, Average IC: 0.0047
-Key Length: 49, Average IC: 0.0044
-Key Length: 50, Average IC: 0.0756
-Key Length: 51, Average IC: 0.0061
-Key Length: 52, Average IC: 0.0057
-Key Length: 53, Average IC: 0.0042
-Key Length: 54, Average IC: 0.0054
-Key Length: 55, Average IC: 0.0149
-Key Length: 56, Average IC: 0.0040
-Key Length: 57, Average IC: 0.0038
-Key Length: 58, Average IC: 0.0047
-Key Length: 59, Average IC: 0.0046
-Key Length: 60, Average IC: 0.0138
+Key Length: 25, Average IC: 0.0752
+Key Length: 26, Average IC: 0.0061
+Key Length: 27, Average IC: 0.0063
+Key Length: 28, Average IC: 0.0057
+Key Length: 29, Average IC: 0.0057
+Key Length: 30, Average IC: 0.0169
+Key Length: 31, Average IC: 0.0056
+Key Length: 32, Average IC: 0.0058
+Key Length: 33, Average IC: 0.0058
+Key Length: 34, Average IC: 0.0056
+Key Length: 35, Average IC: 0.0169
+Key Length: 36, Average IC: 0.0056
+Key Length: 37, Average IC: 0.0056
+Key Length: 38, Average IC: 0.0058
+Key Length: 39, Average IC: 0.0057
+Key Length: 40, Average IC: 0.0173
+Key Length: 41, Average IC: 0.0058
+Key Length: 42, Average IC: 0.0053
+Key Length: 43, Average IC: 0.0054
+Key Length: 44, Average IC: 0.0057
+Key Length: 45, Average IC: 0.0176
+Key Length: 46, Average IC: 0.0058
+Key Length: 47, Average IC: 0.0056
+Key Length: 48, Average IC: 0.0052
+Key Length: 49, Average IC: 0.0058
+Key Length: 50, Average IC: 0.0740
+Key Length: 51, Average IC: 0.0055
+Key Length: 52, Average IC: 0.0055
+Key Length: 53, Average IC: 0.0061
+Key Length: 54, Average IC: 0.0055
+Key Length: 55, Average IC: 0.0167
+Key Length: 56, Average IC: 0.0053
+Key Length: 57, Average IC: 0.0051
+Key Length: 58, Average IC: 0.0050
+Key Length: 59, Average IC: 0.0051
+Key Length: 60, Average IC: 0.0161
 
 Most likely key length: 25
 Recovering key of length 25...
-Recovered Key (hex): 565d68c1678de3e995e354fc062dbd93098ebadaf5f65a9e2e
+Recovered Key (hex): aa06b3a6425d91c75f2cf94567e70d323aa200dfd714d9b640
 
 Decrypted Message:
-*As THeyrOUNdEda bEnd iN thEpaThTHaT Ran BesidE thEriVeR
-                                                        LArA reCogniZed THe SiLHOuEtTe oF a fIg tREe AtOPanEarbY hilL. THE wEaTHErwAs hOt anD thEdaYsWErE Long ThefigTreE WAS InfulL leaF, bUT nOtYEtbEariNg frUit.*sooN lARasPottEd otHer LAndMaRKS⠔An oUtcroPpinGoflIMEsToNe bEsidethePatH THAthAd asilhOuetTE lIkEA MaN's Face,a mARshY SPOtbEsidE therivER wHeREtHewatErfowL weRE eAsILY StArtlEd, atalLtrEeTHaT LookEd liKe aManwITH HiS arMs upRaisED. thEYwErE drAwingneaRtotHEpLaCe wHere TherEwaS ANiSlAnd In thE riVEr.THEiSlAnd Was agooDspOtTO MaKe cAmp. theyWouLdSLeEpon The iSlanDtoNiGHT.*LAra Had bEen BAckaNDfOrTh aLong The RIveR PAThmAny Timesin HEr ShORT LiFe. her pEoplEhaD NOT CrEateD thepatH�ItHAdaLwayS beeN thERe,lIKE ThE riVer‴butTheIrDEeRsKin-Shod FeetAndtHEwOoDen WheelS ofTheIrHAnDcArtskeptthePatH WELlwOrn.Laras pEOplE WEResAlt TradeRs, ANd ThEIR LiVeliHood TookTheM ONacOntiNual JourNEy.*ATThE MoutH of The RIveR,THelIttlE groUp oFhaLfAdOzEn iNtermInglED fAmILIeS GathEred SaltFroM THE GrEat Salt BedsBesIdEThE Sea.TheygroOMedaNDsIfTed The sAlt ANd LoADEdiT inTo haNdcaRTs.WHEN ThE caRts wEre FUll
-      MOStoF thE groUp wOUldsTAY BeHind
-                                        takIng SHelTeRAmIdrocKs anD siMPlelEAN-ToS, wHile A baND oF FIFtEeN orso oF thEheArTIErmEmbeRs seT ouTontHEpAtH thAt raN alONgsIdEThE RiveR.
-WiTh tHEirpRECiOuS caRgo oF saLT, ThETrAvElerS croSsedThecOAStAllowLandsandTraVeLED ToWardthe MounTAinS.bUtLAra'S peoPle NEveR REAcHeD thE mouNtaiNTopS;THeY TravEled OnlyAs FaRAstHe fOoth
+
+As theyrounded a bend In the paTh that ran besiDe the riVer, Lara recognIzed the Silhouette of a Fig tree Atop a nearby hiLl. The wEather was hot aNd the daYs were long. ThE fig treE was in full leAf, but nOt yet bearing fRuit.
+SooN Lara spotted oTher landMarks—an outcrOpping oflimestone besidE the patH that had a silHouette lIke a man's face
+a marshY spot beside thE river wHere the waterfoWl were eAsily startled, A tall trEe that looked lIke a manwith his arms uPraised. they were drawinG near tothe place wherethere waS an island in tHe river.The island was A good spOt to make camp.They wouLd sleep on the Island toNight.
+Lara had Been backand forth alongthe riveR path many timeS in her Short life. Her People haD not created thE path—It had always beEn there,like the river⠔but theIr deerskin-shodfeet andthe wooden wheeLs of theIr handcarts kepT the patH well worn. LarA's peoplE were salt tradErs, and Their livelihoodtook theM on a continualjourney.*At the mouth ofthe riveR, the little grOup of haLf a dozen interMingled fAmilies gatheredsalt froM the great saltbeds besIde the sea. TheY groomedand sifted the Salt and Loaded it into hAndcarts.When the carts Were full
+                                                                                                                                          most of the grOup wouldstay behind, taKing shelTer amid rocks aNd simplelean-tos, whilea band oF fifteen or so Of the heArtier members sEt out onthe path that rAn alongsIde the river.
+WIth theirprecious cargo Of salt, The travelers crOssed thecoastal lowlandS and traVeled toward themountainS. But Lara's peOple neveR reached the moUntaintopS; they traveledonly as Far as the foothIlls. ManY people lived iN the forEsts and grassy Meadows oF the foothills,gatheredin small villagEs. In reTurn for salt, tHese peopLe would give LaRa's peopLe dried meat, aNimal skiNs, cloth spun fRom wool,clay pots, needLes and sCraping tools caRved frombone, and littlE toys maDe of wood.
+TheiR barteriNg done, Lara anD her peoPle would travelback dowN the river pathto the sEa. The cycle woUld beginagain.
+It had aLways beeN like this. LarA knew noother life. Shetraveledback and forth,up and dOwn the river paTh. No siNgle place was hOme. She Liked the seasidE, where There was alwaysfish to Eat, and the genTle lappiNg of the waves Lulled heR to sleep at niGht. She Was less fond ofthe footHills, where thepath greW steep, the nigHts couldbe cold, and viEws of grEat distances maDe her diZzy. She felt unEasy in tHe villages, andwas ofteN shy around strAngers. THe path itself wAs where She felt most athome. ShE loved the smelL of the River on a hot dAy, and tHe croaking of fRogs at nIght. Vines grewamid thelush foliage alOng the rIver, with berriEs that wEre good to eat.Even on The hottest day,sundown Brought a cool bReeze offthe water, whicH sighed And sang amid thE reeds aNd tall grasses.*Of all tHe places along The path,the area they wEre approAching, with theisland iN the river, wasLara's fAvorite.
+The terRain alonG this stretch oF the rivEr was mostly flAt, but iN the immediate Vicinity Of the island, tHe land oN the sunrise siDe was liKe a rumpled cloTh, with Hills and ridgesand vallEys. Among Lara'S people,there was a wooDen baby'S crib, suitablefor straPping to a cart,that hadbeen passed dowN for genErations. The isLand was Shaped like thatcrib, loNger than it waswide andpointed at the Upriver eNd, where the flOw had erOded both b
 '''
